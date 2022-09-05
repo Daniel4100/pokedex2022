f7613ab737b5d6c836ce0f65882cc75ce25edffd
Jhener Daniel <jhener990@gmail.com>
2022-09-04 19:15:20 -0500

    the end

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,7 @@
 import { getName } from "../../store/slices/nameTrainer.slice";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import "./home.css";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -19,13 +20,25 @@ const Home = () => {
   };
 
   return (
-    <div>
-      <h1>hi triner!</h1>
-      <p>To star </p>
-      <form onSubmit={handleSubmit}>
-        <input id="name" type="text" />
-        <button>catch them all</button>
-      </form>
+    <div className="home">
+      <div className="container__home">
+
+      <div className="home__video-conatainer">
+        <video autoPlay muted loop  src="./media/videoHome.mp4"></video>
+      </div>
+      <div className="home__form-container">
+        <h1>hi triner!</h1>
+        <p>to get started, put your trainer name</p>
+        <form onSubmit={handleSubmit}>
+          <input required maxLength={10} id="name" type="text" placeholder="0 - 10 character" />
+          <button className="custom-btn btn-11">catch them all</button>
+        </form>
+        <img className="home__png" src="./media/home.png" alt="" />
+      </div>
+      </div>
+
+
+      
     </div>
   );
 };
